c8ec78a0973d6d429925fb4d030bd166f5274bc0
Joel Caban <joel.caban2017@gmail.com>
2024-06-12 15:05:15 -0400

    update

--- model/amenities.py
+++ model/amenities.py
@@ -1,7 +1,12 @@
+#!/usr/bin/python3
+from model.base_model import BaseModel
 
+class Amenities(BaseModel):
+    amenities = []
 
-class Amenities:
-    def __init__(self, wifi, pools, user_add):
-        self.wifi = wifi
-        self.pools = pools
-        self.user_add = user_add
+    def __init__(self, name, **kwargs):
+        super().__init__(**kwargs)
+        if name in [amenity.name for amenity in Amenities.amenities]:
+            raise ValueError("Amenity name must be unique")
+        self.name = name
+        Amenities.amenities.append(self)
