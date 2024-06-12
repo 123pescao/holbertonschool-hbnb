d1b127399af71f06c0b438a52935a4e880338be9
Joel Caban <joel.caban2017@gmail.com>
2024-06-12 15:07:51 -0400

    update

--- model/base_model.py
+++ model/base_model.py
@@ -1,8 +1,10 @@
+#!/usr/bin/python3
+print("Loading BaseModel from base_model.py")
 from datetime import datetime
 import uuid
 
 
-class Basemodel:
+class BaseModel:
     """
     BaseModel class that serves as a base for other models, providing unique ID
     creation timestamp, and update timestamp.
@@ -10,11 +12,6 @@ class Basemodel:
     def __init__(self, id=None, created_at=None, updated_at=None):
         """
         Initialize a new instance of BaseModel.
-
-        Args:
-            id (str, optional): Defaults to a new Unique UUID4 string.
-            created_at (datetime, optional): Timestamp when created.
-            updated_at (datetime, optional): Timestamp when last updated.
         """
         self.id = id or str(uuid.uuid4())
         self.created_at = created_at or datetime.now()
